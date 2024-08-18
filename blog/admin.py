f154072a3d4fdc16fada4a69a5a1c39817c8f4c4
Basel Hussein <cv98@windowslive.com>
2024-08-18 15:17:55 +0000

    added comments model and admin

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,22 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Comment
 
-# Register your models here.
-admin.site.register(Post)
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'created_on', 'status')
+    list_filter = ('status', 'created_on')
+    search_fields = ('title', 'content')
+    prepopulated_fields = {'slug': ('title',)}
+    raw_id_fields = ('author',)
+    date_hierarchy = 'created_on'
+    ordering = ('status', 'created_on')
+
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'post', 'created_on', 'active')
+    list_filter = ('active', 'created_on')
+    search_fields = ('name', 'email', 'body')
+    actions = ['approve_comments']
+
+    def approve_comments(self, request, queryset):
+        queryset.update(active=True)
