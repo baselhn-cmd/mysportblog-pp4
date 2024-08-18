1852c6b5fb7fdf80a8f8e6ee558282196bd9180a
Basel Hussein <cv98@windowslive.com>
2024-08-18 13:19:09 +0000

    added secret key2

--- sportblog/settings.py
+++ sportblog/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-7(@w6gkgu15*au33awt$0llkl4a#7rl^*&ly8gz-rbrkwbw505'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
