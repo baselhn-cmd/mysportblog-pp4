e5069c232cc407ffe9d765c1f251f0b7d17aa660
Basel Hussein <cv98@windowslive.com>
2024-08-23 13:52:10 +0000

    added comment views and template

---
+++ blog/forms.py
@@ -0,0 +1,25 @@
+from .models import Post, Comment
+from django import forms
+from django_summernote.widgets import SummernoteWidget
+
+
+# Note: the code has been used from the CI tutorial
+# I Think Therefore I Blog
+# to help the setup and creation of this project.
+
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        exclude = ('likes',)
+        widgets = {
+            'content': SummernoteWidget(),
+        }
+
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ('body',)
+
+
