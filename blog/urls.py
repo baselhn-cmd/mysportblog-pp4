8125c1b437bcc622967da80d8d07898fd91e3218
Basel Hussein <cv98@windowslive.com>
2024-09-04 16:22:44 +0000

    changing views

--- blog/urls.py
+++ blog/urls.py
@@ -4,12 +4,12 @@ from . import views
 urlpatterns = [
     path('', views.PostList.as_view(), name='home'),
     path('add_post/', views.AddPost.as_view(), name='add_post'),
-    
-    path('post/<slug:slug>/edit/', views.EditPost.as_view(), name='edit_post'),
+    path('<slug:slug>/edit/', views.EditPost.as_view(), name='edit_post'),
+
     path('post/<slug:slug>/delete/', views.DeletePost.as_view(), name='delete_post'),
     path('<slug:slug>/', views.post_detail, name="post_detail"),
-    
     path('<slug:slug>/edit_comment/<int:comment_id>/', views.comment_edit, name='comment_edit'),
     path('<slug:slug>/delete_comment/<int:comment_id>/', views.comment_delete, name='comment_delete'),
     path('like/<slug:slug>/', views.PostLike.as_view(), name='post_like'),
+
 ]
--- blog/views.py
+++ blog/views.py
@@ -7,6 +7,8 @@ from django.http import HttpResponseRedirect
 from .models import Post, Comment
 from .forms import CommentForm, PostForm
 from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin
+
 
 class PostList(generic.ListView):
     queryset = Post.objects.filter(status=1).order_by("-created_on")
@@ -93,14 +95,13 @@ class DeletePost(generic.DeleteView):
     def get_object(self, queryset=None):
         slug = self.kwargs.get('slug')
 
-class PostLike(View):
+class PostLike(LoginRequiredMixin, View):
     def post(self, request, slug, *args, **kwargs):
-        post = get_object_or_404(Post, slug=slug)
-        if post.likes.filter(id=request.user.id).exists():
+        post = get_object_or_404(Post, slug=self.kwargs.get('slug'))
+        if post.likes.all().filter(id=request.user.id).exists():
             post.likes.remove(request.user)
         else:
             post.likes.add(request.user)
-
         return HttpResponseRedirect(reverse('post_detail', args=[slug]))
 
 @login_required
