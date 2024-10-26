61edb7e95f52a91641c075f8d5fbc38dc642f118
Basel Hussein <cv98@windowslive.com>
2024-10-26 11:18:31 +0000

    created new env.py

--- sportblog/settings.py
+++ sportblog/settings.py
@@ -32,10 +32,11 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["8000-baselhncmd-mysportblogp-d8y0zsk86yx.ws.codeinstitute-ide.net",
-                '.herokuapp.com',"8000-baselhncmd-mysportblogp-xst0ssx5nb0.ws.codeinstitute-ide.net"]
+                '.herokuapp.com',"8000-baselhncmd-mysportblogp-xst0ssx5nb0.ws.codeinstitute-ide.net",
+                '8000-baselhncmd-mysportblogp-d7orar04qb4.ws.codeinstitute-ide.net']
 
 
 # Application definition
