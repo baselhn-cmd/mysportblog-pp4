e5876ef072f8f12a48e33eb5eb67c6bd73c49815
Basel Hussein <cv98@windowslive.com>
2024-11-02 12:54:05 +0000

    resolve issue in update comment

--- sportblog/settings.py
+++ sportblog/settings.py
@@ -49,7 +49,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # 'cloudinary_storage',
+    'cloudinary_storage',
     'django.contrib.sites',
     'allauth',
     'allauth.account',
